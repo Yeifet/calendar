1441908ae1068a1c9ed6bb7fa307531704c15aa8
yeifet <yeifettoledo@gmail.com>
2022-11-09 09:20:11 -0500

    Calendar Proyect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const locales = {
+  'es-ES': require('date-fns/locale/es')
 }
 
-export default App;
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [
+  { title: 'Big Meeting', allDay: true, start: new Date(2022, 11, 0), end: new Date(2022, 11, 0) },
+  { title: 'Vacation', allDay: true, start: new Date(2022, 11, 7), end: new Date(2022, 11, 10) },
+  { title: 'Conference', allDay: true, start: new Date(2022, 11, 20), end: new Date(2022, 11, 23) }
+]
+
+function App () {
+  const [newEvent, setNewEvent] = useState({ title: '', start: '', end: '' })
+  const [allEvents, setAllEvents] = useState(events)
+
+  const handleAddEvent = () => {
+    setAllEvents([...allEvents, newEvent])
+  }
+
+  return (
+    <div className="App">
+      <h1>Calendar</h1>
+      <h2>Add New Event</h2>
+      <div>
+        <input type='text' placeholder='Add title' className='w-2/3 mr-5'
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <DatePicker placeholderText='Start Date' style={{ marginRight: '10px' }}
+        selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
+        <DatePicker placeholderText='End Date'
+        selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
+      </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 500, margin: '50px' }}
+      />
+    </div>
+  )
+}
+
+export default App
